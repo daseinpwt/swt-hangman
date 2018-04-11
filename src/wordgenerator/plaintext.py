1d5ab79278dd537b71e9d5394e0747de31e91ca6
Jay <sanjaysheombar@gmail.com>
2018-04-12 00:24:45 +0200

    Changed indentation to spaces

--- src/wordgenerator/plaintext.py
+++ src/wordgenerator/plaintext.py
@@ -4,16 +4,16 @@ from random import randint
 
 class Plaintext(BaseWordGenerator):
 
-	def get_word(self, filename):
-		words = self.generate_words(filename)
-		return list(words[randint(0, len(words)-1)])
+    def get_word(self, filename):
+        words = self.generate_words(filename)
+        return list(words[randint(0, len(words)-1)])
 
-	def generate_words(self, filename):
-		file = open(self.format_filename(filename), "r")
-		words = file.read().splitlines()
-		return words
+    def generate_words(self, filename):
+        file = open(self.format_filename(filename), "r")
+        words = file.read().splitlines()
+        return words
 
-	def format_filename(self, filename):
-		extention = ".txt"
-		dir_path = os.path.dirname(os.path.realpath(__file__))
-		return "{}/text/{}{}".format(dir_path, filename, extention)
+    def format_filename(self, filename):
+        extention = ".txt"
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        return "{}/text/{}{}".format(dir_path, filename, extention)
