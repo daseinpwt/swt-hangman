3da82053cfe62fe73471047a7dfcf5ccc1ec6080
Jay <sanjaysheombar@gmail.com>
2018-04-11 19:08:58 +0200

    Add reading from plaintext

--- src/wordgenerator/base.py
+++ src/wordgenerator/base.py
@@ -2,9 +2,6 @@ from abc import ABCMeta, abstractmethod
 
 class BaseWordGenerator(metaclass=ABCMeta):
 
-    def __init__(self):
-        pass
-
     @abstractmethod
     def get_word(self):
         pass
