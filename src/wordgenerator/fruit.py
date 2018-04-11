3e5d248f5b340d808497c890d1757c867f253671
Jay <sanjaysheombar@gmail.com>
2018-04-11 14:32:36 +0200

    Change wordschatz to wordgenerator

---
+++ src/wordgenerator/fruit.py
@@ -0,0 +1,26 @@
+from .base import BaseWordGenerator
+from random import randint
+
+class Fruit(BaseWordGenerator):
+
+	def get_word(self):
+		words = self.generate_words()
+		return list(words[randint(0, len(words)-1)])
+
+	def generate_words(self):
+		words = [
+			"apple",
+			"lemon",
+			"banana",
+			"strawberry",
+			"pineapple",
+			"peach",
+			"kiwi",
+			"grape",
+			"orange",
+			"cherry",
+			"melon",
+			"grapefruit",
+		]
+
+		return words
--- src/wortschatz/base.py
+++
@@ -1,6 +0,0 @@
-class BaseWortschatz:
-    def __init__(self):
-        pass
-
-    def newWord(self):
-        return 'apple'
