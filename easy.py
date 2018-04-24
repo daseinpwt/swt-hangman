7468f4b18cf2d23e25f1c4d3aa10ab2f80f5192e
Bas van der Borden <bascasper@gmail.com>
2018-04-24 10:09:34 +0200

    Fixed \ in print statements

---
+++ easy.py
@@ -0,0 +1,264 @@
+class BasePainter:
+    def __init__(self, maxNumFails):
+        self.maxNumFails = maxNumFails
+
+    def draw_current_state(self, guess, word, numFails):
+        if numFails == 0:
+            print()
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('The word: \n\t', end = '')
+            for i, c in enumerate(word):
+                if guess[i]:
+                    print(c, end = '')
+                else:
+                    print('_', end = '')
+            print()
+            print()
+        elif numFails == 1:
+            print()
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('.|.......|.')
+            print('The word: \n\t', end = '')
+            for i, c in enumerate(word):
+                if guess[i]:
+                    print(c, end = '')
+                else:
+                    print('_', end = '')
+            print()
+            print()
+        elif numFails == 2:
+            print()
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('._________.')
+            print('.|.......|.')
+            print('The word: \n\t', end = '')
+            for i, c in enumerate(word):
+                if guess[i]:
+                    print(c, end = '')
+                else:
+                    print('_', end = '')
+            print()
+            print()
+        elif numFails == 3:
+            print()
+            print('...........')
+            print('.........|.')
+            print('.........|.')
+            print('.........|.')
+            print('.........|.')
+            print('.........|.')
+            print('.........|.')
+            print('._________.')
+            print('.|.......|.')
+            print('The word: \n\t', end = '')
+            for i, c in enumerate(word):
+                if guess[i]:
+                    print(c, end = '')
+                else:
+                    print('_', end = '')
+            print()
+            print()
+        elif numFails == 4:
+            print()
+            print('.---------.')
+            print('.........|.')
+            print('.........|.')
+            print('.........|.')
+            print('.........|.')
+            print('.........|.')
+            print('.........|.')
+            print('._________.')
+            print('.|.......|.')
+            print('The word: \n\t', end = '')
+            for i, c in enumerate(word):
+                if guess[i]:
+                    print(c, end = '')
+                else:
+                    print('_', end = '')
+            print()
+            print()
+        elif numFails == 5:
+            print()
+            print('.---------.')
+            print('.......\\.|.')
+            print('........\\|.')
+            print('.........|.')
+            print('.........|.')
+            print('.........|.')
+            print('.........|.')
+            print('._________.')
+            print('.|.......|.')
+            print('The word: \n\t', end = '')
+            for i, c in enumerate(word):
+                if guess[i]:
+                    print(c, end = '')
+                else:
+                    print('_', end = '')
+            print()
+            print()
+        elif numFails == 6:
+            print()
+            print('.---------.')
+            print('.......\\.|.')
+            print('....O...\\|.')
+            print('.........|.')
+            print('.........|.')
+            print('.........|.')
+            print('.........|.')
+            print('._________.')
+            print('.|.......|.')
+            print('The word: \n\t', end = '')
+            for i, c in enumerate(word):
+                if guess[i]:
+                    print(c, end = '')
+                else:
+                    print('_', end = '')
+            print()
+            print()
+        elif numFails == 7:
+            print()
+            print('.---------.')
+            print('.......\\.|.')
+            print('....O...\\|.')
+            print('....|....|.')
+            print('....|....|.')
+            print('.........|.')
+            print('.........|.')
+            print('._________.')
+            print('.|.......|.')
+            print('The word: \n\t', end = '')
+            for i, c in enumerate(word):
+                if guess[i]:
+                    print(c, end = '')
+                else:
+                    print('_', end = '')
+            print()
+            print()
+        elif numFails == 8:
+            print()
+            print('.---------.')
+            print('.......\\.|.')
+            print('....O...\\|.')
+            print('....|/...|.')
+            print('....|....|.')
+            print('.........|.')
+            print('.........|.')
+            print('._________.')
+            print('.|.......|.')
+            print('The word: \n\t', end = '')
+            for i, c in enumerate(word):
+                if guess[i]:
+                    print(c, end = '')
+                else:
+                    print('_', end = '')
+            print()
+            print()
+        elif numFails == 9:
+            print()
+            print('.---------.')
+            print('.......\\.|.')
+            print('....O...\\|.')
+            print('...\\|/...|.')
+            print('....|....|.')
+            print('.........|.')
+            print('.........|.')
+            print('._________.')
+            print('.|.......|.')
+            print('The word: \n\t', end = '')
+            for i, c in enumerate(word):
+                if guess[i]:
+                    print(c, end = '')
+                else:
+                    print('_', end = '')
+            print()
+            print()
+        elif numFails == 10:
+            print()
+            print('.---------.')
+            print('.......\\.|.')
+            print('....O...\\|.')
+            print('...\\|/...|.')
+            print('....|....|.')
+            print('.....\\...|.')
+            print('.........|.')
+            print('._________.')
+            print('.|.......|.')
+            print('The word: \n\t', end = '')
+            for i, c in enumerate(word):
+                if guess[i]:
+                    print(c, end = '')
+                else:
+                    print('_', end = '')
+            print()
+            print()
+        elif numFails == 11:
+            print()
+            print('.---------.')
+            print('.......\\.|.')
+            print('....O...\\|.')
+            print('...\\|/...|.')
+            print('....|....|.')
+            print('.../.\\...|.')
+            print('.........|.')
+            print('._________.')
+            print('.|.......|.')
+            print('The word: \n\t', end = '')
+            for i, c in enumerate(word):
+                if guess[i]:
+                    print(c, end = '')
+                else:
+                    print('_', end = '')
+            print()
+            print()
+            
+    def draw_win_state(self, word, numFails):
+        print()
+        print('Congratulations! You win!')
+        print('The word is:\n\t', end = '')
+        print(word)
+        print()
+        print()
+        print('...........')
+        print('....YOU....')
+        print('....ARE....')
+        print('.....A.....')
+        print('...CHAMP...')
+        print('...........')
+        print()
+
+    def draw_lose_state(self, guess, word):
+        print()
+        print('..-------..')
+        print('....|...|..')
+        print('....X...|..')
+        print('...\\|/..|..')
+        print('....|...|..')
+        print('.../.\\..|..')
+        print('........|..')
+        print('You Lose!')
+        print('The word is:\n\t', end = '')
+        print(word)
+        print()
+
+    def get_new_guess(self):
+        return input('Please guess a new character: ')
---
+++ hard.py
@@ -0,0 +1,154 @@
+class BasePainter:
+    def __init__(self, maxNumFails):
+        self.maxNumFails = maxNumFails
+
+    def draw_current_state(self, guess, word, numFails):
+        if numFails == 0:
+            print()
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('The word: \n\t', end = '')
+            for i, c in enumerate(word):
+                if guess[i]:
+                    print(c, end = '')
+                else:
+                    print('_', end = '')
+            print()
+            print()
+        elif numFails == 1:
+            print()
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('...........')
+            print('._________.')
+            print('.|.......|.')
+            print('The word: \n\t', end = '')
+            for i, c in enumerate(word):
+                if guess[i]:
+                    print(c, end = '')
+                else:
+                    print('_', end = '')
+            print()
+            print()
+        elif numFails == 2:
+            print()
+            print('..-------..')
+            print('......\\.|..')
+            print('.......\\|..')
+            print('........|..')
+            print('........|..')
+            print('........|..')
+            print('........|..')
+            print('._________.')
+            print('.|.......|.')
+            print('The word: \n\t', end = '')
+            for i, c in enumerate(word):
+                if guess[i]:
+                    print(c, end = '')
+                else:
+                    print('_', end = '')
+            print()
+            print()
+        elif numFails == 3:
+            print()
+            print('..-------..')
+            print('......\\.|..')
+            print('....O..\\|..')
+            print('....|...|..')
+            print('....|...|..')
+            print('........|..')
+            print('........|..')
+            print('._________.')
+            print('.|.......|.')
+            print('The word: \n\t', end = '')
+            for i, c in enumerate(word):
+                if guess[i]:
+                    print(c, end = '')
+                else:
+                    print('_', end = '')
+            print()
+            print()
+        elif numFails == 4:
+            print()
+            print('..-------..')
+            print('......\\.|..')
+            print('....O..\\|..')
+            print('...\\|/..|..')
+            print('....|...|..')
+            print('........|..')
+            print('........|..')
+            print('._________.')
+            print('.|.......|.')
+            print('The word: \n\t', end = '')
+            for i, c in enumerate(word):
+                if guess[i]:
+                    print(c, end = '')
+                else:
+                    print('_', end = '')
+            print()
+            print()
+        elif numFails == 5:
+            print()
+            print('..-------..')
+            print('......\\.|..')
+            print('....O..\\|..')
+            print('...\\|/..|..')
+            print('....|...|..')
+            print('.../.\\..|..')
+            print('........|..')
+            print('._________.')
+            print('.|.......|.')
+            print('The word: \n\t', end = '')
+            for i, c in enumerate(word):
+                if guess[i]:
+                    print(c, end = '')
+                else:
+                    print('_', end = '')
+            print()
+            print()
+
+    def draw_win_state(self, word, numFails):
+        print()
+        print('Congratulations! You win!')
+        print('The word is:\n\t', end = '')
+        print(word)
+        print()
+        print()
+        print('...........')
+        print('....YOU....')
+        print('....ARE....')
+        print('.....A.....')
+        print('...CHAMP...')
+        print('...........')
+        print()
+
+    def draw_lose_state(self, guess, word):
+        print()
+        print('..-------..')
+        print('....|.\\.|..')
+        print('....X..\\|..')
+        print('...\\|/..|..')
+        print('....|...|..')
+        print('.../.\\..|..')
+        print('........|..')
+        print('._________.')
+        print('.|.......|.')
+        print('You Lose!')
+        print('The word is:\n\t', end = '')
+        print(word)
+        print()
+
+    def get_new_guess(self):
+        return input('Please guess a new character: ')
